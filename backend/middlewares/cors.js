3371fd55fa4033b0c7ad1e1d84cad3770fcc1c1a
zubastu <79757684+zubastu@users.noreply.github.com>
2022-06-29 12:53:30 +0300

    подфиксил cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,8 @@
 const allowedCors = [
-  'http://localhost:3000',
-  'https://localhost:3000',
   'http://api.zubastu.nomoreparties.sbs',
   'https://api.zubastu.nomoreparties.sbs',
+  'http://localhost:3000',
+  'https://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
@@ -11,14 +11,17 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
+
   res.header('Access-Control-Allow-Credentials', true);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.end();
   }
-  return next();
+  next();
 };
